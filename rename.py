610c850deba89ce14024e7baa71bcceee03d5e0b
Vishesh Garg <visheshgarg5226@gmail.com>
2020-11-21 16:18:05 +0530

    add option to different extentions

--- rename.py
+++ rename.py
@@ -1,9 +1,10 @@
 import os
 
-file_path = r"<directory path>"
+file_path = r"C:\Users\HP\Desktop\imp documents"
 
+Q = int(input("Press 1 to convert in png or 2 to convert in jpg : "))
 
-def rename_file(file_path):
+if 	Q == 1:
 	os.chdir(file_path)
 	i= 0
 	for file in os.listdir():
@@ -27,5 +28,26 @@ def rename_file(file_path):
 		os.rename(file , newname)
 		print(i,"Done")
 
-rename_file(file_path)
+elif Q==2:
+	os.chdir(file_path)
+	i= 0
+	for file in os.listdir():
+		i+=1
+		filename, fileext = os.path.splitext(file)
+
+		if fileext == ".png": 
+			fileext = ".jpg"
+
+		else:
+			continue
+			
+		newname = '{}{}'.format(filename,fileext)
+
+		os.rename(file , newname)
+		print(i,"Done")
+
+else:
+	print("Invail extension")
+	
+
 
